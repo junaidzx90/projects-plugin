25acf2d493d5be5794e154b4aab048d036e79e43
junaidzx90 <jsjunu@gmail.com>
2021-05-10 23:22:46 +0600

    5

--- projects-plugin.php
+++ projects-plugin.php
@@ -9,14 +9,14 @@
  * that starts the plugin.
  *
  * @link              peterkeyser.ca
- * @since             1.0.0
+ * @since             1.0.1
  * @package           Projects_Plugin
  *
  * @wordpress-plugin
  * Plugin Name:       Projects Plugin
  * Plugin URI:        peterkeyser.ca/projects-plugin
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Peter Keyser
  * Author URI:        peterkeyser.ca
  * License:           GPL-2.0+
@@ -52,10 +52,10 @@ function pp_project_admin_noticess(){
 
 /**
  * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Start at version 1.0.1 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PROJECTS_PLUGIN_VERSION', '1.0.0' );
+define( 'PROJECTS_PLUGIN_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
@@ -91,7 +91,7 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-projects-plugin.php';
  * then kicking off the plugin from this point in the file does
  * not affect the page life cycle.
  *
- * @since    1.0.0
+ * @since    1.0.1
  */
 function run_projects_plugin() {
 
